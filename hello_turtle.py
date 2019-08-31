671f76eff45b7e29f57090585de575d62468eee1
Woody Hooten <frymatic@woodyhooten.com>
2019-08-30 21:32:01 -0700

    Create hello_turtle.py

---
+++ hello_turtle.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> myTurtle = turtle.Turtle()
+>>> myTurtle.color('cyan')
+>>> radius = 10
+>>> myTurtle.forward(radius)
+>>> radius = 100
+>>> myTurtle.forward(radius)
+>>> myTurtle.left(1)
+>>> myTurtle.left(89)
+>>> myTurtle.circle(50)
+>>> myTurtle.circle(-50)
+>>> turtle.getscreen()._root.mainloop()
